d7035b1ba4b168f9e253487df216fd86c8e548b4
Nisaba <cbernardini@nisaba.solutions>
2026-03-25 20:57:53 +0000

    update plugin to .NET 10

--- BTCPayServer.Plugins.MtPelerin/Plugin.cs
+++ BTCPayServer.Plugins.MtPelerin/Plugin.cs
@@ -1,26 +1,21 @@
 ﻿using BTCPayServer.Abstractions.Contracts;
 using BTCPayServer.Abstractions.Models;
-using BTCPayServer.Abstractions.Services;
+using BTCPayServer.Plugins.MtPelerin.Data;
 using BTCPayServer.Plugins.MtPelerin.Services;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BTCPayServer.Plugins.MtPelerin;
 
 public class Plugin : BaseBTCPayServerPlugin
 {
-    public override string Identifier => "mtpelerin";
-    public override string Name => "MtPelerin";
-
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     {
-        new IBTCPayServerPlugin.PluginDependency { Identifier = nameof(BTCPayServer), Condition = ">=2.0.1" }
+        new IBTCPayServerPlugin.PluginDependency { Identifier = nameof(BTCPayServer), Condition = ">=2.3.7" }
     };
 
     public override void Execute(IServiceCollection services)
     {
         services.AddUIExtension("store-wallets-nav", "MtPelerinPluginHeaderNav")       
-                .AddHostedService<ApplicationPartsLogger>()
                 .AddHostedService<PluginMigrationRunner>()
                 .AddSingleton<MtPelerinPluginDbContextFactory>()
                 .AddSingleton<MtPelerinPluginService>();
